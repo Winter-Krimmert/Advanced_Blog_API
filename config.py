4ebe28b08a4d76096cd09dba4678b9c216921a6b
Winter-Krimmert <winterk463@gmail.com>
2024-07-21 01:56:14 -0700

    Added Postgresql

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import datetime  # Import the datetime module
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -8,11 +7,16 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(24)
     
-
-    # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or os.getenv('SQLALCHEMY_DATABASE_URL')
+    # Determine the database URI to use
+    DATABASE_TYPE = os.getenv('DATABASE_TYPE', 'sqlite').lower()
+    
+    if DATABASE_TYPE == 'sqlite':
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite:///default.db'
+    elif DATABASE_TYPE == 'mysql':
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'mysql+mysqlconnector://root:password@localhost/dbname'
+    elif DATABASE_TYPE == 'postgresql':
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'postgresql://user:password@localhost/dbname'
+    else:
+        raise ValueError(f"Unsupported database type: {DATABASE_TYPE}")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
